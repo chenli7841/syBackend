e47cd127aca6bf89350db0161f3bc51cbf3810ca
chenli7841 <li.chen7841@gmail.com>
2026-07-03 18:15:06 -0400

    code cleanup for claude code

--- DB Scripts/update_scripts.sql
+++
@@ -1,4 +0,0 @@
-update transport_order o
-join pick_up_location p on o.pick_up_location_id=p.id
-set o.DistrictAdditionalCost = o.WeightKg * p.district_additional_cost
-where o.DistrictAdditionalCost = 0 and p.district_additional_cost > 0 and o.WeightKg > 0 and o.Id > 0
